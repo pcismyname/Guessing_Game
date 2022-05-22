32c3f2b7870c2454b98c471316bb1c5831ba4e15
pcismynme <pcismyname007@gmail.com>
2022-05-22 12:29:09 +0700

    add server gui

--- client.py
+++ client.py
@@ -15,6 +15,50 @@ def exit_b(window):
 
 def new_game():
 
+    def reset():
+        game_win.destroy()
+        new_game()
+
+    def winner(_server_data):
+        canvas_game.itemconfig(server_print, text="You Won")
+        turn = _server_data[-1]
+        canvas_game.itemconfig(attempt, text=("Attempt: " + turn))
+        b_submit.place_forget()
+
+        b_new_game_l = Button(game_win, text="N E W  G A M E", height=2, width=26, bg="#d262f9", relief="raised",
+                              activebackground="#7e3a95", state=NORMAL, font="Times 16", command=reset)
+        b_new_game_l.place(x=400, y=400, anchor="center")
+        place_score = user_name+" "+turn+"\n\n"
+        f = open("scoreBoard.txt", "a")
+        f.write(place_score)
+        f.close()
+
+    def send_to_server():
+        user_data = user_entry.get()
+        tcpSocket.send(user_data.encode())
+        server_data = tcpSocket.recv(2048).decode()
+        user_entry.delete(0, END)
+
+        if "Lost" in server_data:
+            canvas_game.itemconfig(server_print, text="You Lost")
+            b_submit.place_forget()
+            b_new_game_l = Button(game_win, text="N E W  G A M E", height=2, width=26, bg="#d262f9", relief="raised",
+                                  activebackground="#7e3a95", state=NORMAL, font="Forte 16", command=reset)
+            b_new_game_l.place(x=400, y=400, anchor="center")
+        elif "Win" in server_data:
+            winner(server_data)
+        elif "High" in server_data:                                                     
+            canvas_game.itemconfig(server_print, text="Too High")
+            turn = server_data[-1]
+            canvas_game.itemconfig(attempt, text=("Attempt: "+turn))
+        elif "Low" in server_data:                                                      
+            canvas_game.itemconfig(server_print, text="Too Low")
+            turn = server_data[-1]
+            canvas_game.itemconfig(attempt, text=("Attempt: " + turn))
+        else:
+            canvas_game.itemconfig(server_print, text=server_data)
+
+
     b_new_game["state"] = "disable"
     b_score["state"] = "disable"
 
@@ -34,29 +78,31 @@ def new_game():
     server_print = canvas_game.create_text(400, 180, text="Guess Number in range\n1-20", fill="white",
                                 font="Times 34", justify="center", anchor="n")
     
-    user_entry = Entry(game_win, width=4, font="Forte 26 bold", justify="center", bg="#df91fa")
+    user_entry = Entry(game_win, width=4, font="Times 26 bold", justify="center", bg="#df91fa")
     canvas_game.create_window(400, 320,  window=user_entry)
 
-    b_submit = Button(game_win, text="S U B M I T", height=2, width=26, bg="#d262f9", relief="raised",  
-                      activebackground="#7e3a95", state=NORMAL, font="Forte 16", command=send_to_server)
+    b_submit = Button(game_win, text="SUBMIT", height=2, width=26, bg="#d262f9", relief="raised",  
+                      activebackground="#7e3a95", state=NORMAL, font="Times 16", command=send_to_server)
 
     b_submit.place(x=400, y=400, anchor="center")
     b_submit.bind("<Enter>", hover_in)
     b_submit.bind("<Leave>", hover_out)
 
-    def reset():
-        game_win.destroy()
-        new_game()
+    b_exit_score = Button(game_win, text="EXIT", height=2, width=26, bg="#d262f9", relief="raised",    # Exit Button
+                          activebackground="#7e3a95", command=lambda:exit_b(game_win), state=NORMAL, font="Times 16")
 
-    def winner(_server_data):
-        canvas_game.itemconfig(server_print, text="You Won")
+    b_exit_score.place(x=400, y=530, anchor="center")
+    b_exit_score.bind("<Enter>", hover_in)
+    b_exit_score.bind("<Leave>", hover_out)
 
-    def send_to_server():
-        user_data = user_entry.get()
+    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)                               # Connection to socket
+    tcpSocket.connect(("127.0.0.1", 8000))
+
 
 
 
 def score_board():
+
     main_win.iconify()
     f = open("scoreBoard.txt", "r")
     scoreList = f.read()
@@ -69,12 +115,26 @@ def score_board():
     canvas_score.pack(fill="both",expand=True)
     canvas_score.create_image(0, 0, image=bg_score, anchor="nw")
 
+    canvas_score.create_text(240, 100, text="S C O R E\nB O A R D",
+                             fill="#d262f9", font="Times 30 bold",
+                             justify="center")
+    canvas_score.create_text(240, 240, text=scoreList,
+                             fill="white", font="Forte 18",
+                             justify="center", anchor="n")
+    b_exit_score = Button(score_window, text="E X i T", height=2, width=26, bg="#d262f9",
+                          relief="raised", activebackground="#7e3a95",
+                          command=lambda: exit_b(score_window), state=NORMAL, font="Times 16")
 
-def hover_in(e):                                                          # Cursor over button effect / in
+    b_exit_score.place(x=240, y=580, anchor="center")
+    b_exit_score.bind("<Enter>", hover_in)
+    b_exit_score.bind("<Leave>", hover_out)
+
+
+def hover_in(e):                                                         
     e.widget["background"] = "#FA9F42"
 
 
-def hover_out(e):                                                         # Cursor over button effect / out
+def hover_out(e):                                                         
     e.widget["background"] = "#721817"
 
 
@@ -124,7 +184,7 @@ main_win.title("Guess Number")
 
 user_name = ""
 bg_main = PhotoImage(file="img/bg-math.png")
-bg_score = PhotoImage(file="img/score.png")
+bg_score = PhotoImage(file="img/bg-score.png")
 bg_login = PhotoImage(file="img/login.png")
 bg_game = PhotoImage(file="img/game.png")
 
@@ -142,15 +202,15 @@ b_exit = Button(canvas_main, text="EXIT", height=2, width=26, bg="#721817", reli
                 activebackground="#721817", command=main_win.destroy, state=NORMAL, font="Times 16")
 
 
-b_new_game.place(x=320, y=220, anchor="center")
+b_new_game.place(x=320, y=220, anchor=CENTER)
 b_new_game.bind("<Enter>", hover_in)
 b_new_game.bind("<Leave>", hover_out)
 
-b_score.place(x=320, y=290, anchor="center")
+b_score.place(x=320, y=290, anchor=CENTER)
 b_score.bind("<Enter>", hover_in)
 b_score.bind("<Leave>", hover_out)
 
-b_exit.place(x=320, y=380, anchor="center")         # Button place
+b_exit.place(x=320, y=380, anchor=CENTER)         
 b_exit.bind("<Enter>", hover_in)
 b_exit.bind("<Leave>", hover_out)
 
--- server.py
+++ server.py
@@ -1,39 +1,100 @@
+from tkinter import *
 import socket
 import random
+import threading
 
-tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-tcpSocket.bind(("",8000))
 
-while True:
-    tcpSocket.listen(1)
-    (client, (ip, port)) = tcpSocket.accept()
-    print("connect")
 
-    num = random.randint(1, 20)
-    print("Random Number is", num)
-    turn = 0
-    while turn <= 5:
+def start_server():
+    global tcpSocket
+    tcpSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+    tcpSocket.bind(("",8000))
+    threading._start_new_thread(start_game, ())
+    start_btn["state"] = "disable"
 
-        try:
-            turn += 1
-            data = int(client.recv(2048).decode())
-            if int(data) == num:
-                client.send("win".encode()+str(turn).encode())
+def stop_server():
+    main_win.destroy()
+
+
+def start_game():
+    global tcpSocket
+    count = 0
+    while True:
+        tcpSocket.listen(1)
+        (client, (ip, port)) = tcpSocket.accept()
+        print("connect")
+        canvas_main.create_text(60, 100+count*15, text="client connect", fill="#FA9F42",
+                        font="Times 15 bold",justify="center",anchor=N)
+        num = random.randint(1, 20)
+        print("Random Number is", num)
+        canvas_main.create_text(60, 100+count*20, text="client connect", fill="#FA9F42",
+                        font="Times 15 bold",justify="center",anchor=N)
+        turn = 0
+        count += 1
+        while turn <= 5:
+
+            try:
+                turn += 1
+                data = int(client.recv(2048).decode())
+                if int(data) == num:
+                    client.send("Win".encode()+str(turn).encode())
+                    break
+                if int(data) > num:
+                    if turn == 5:
+                        client.send("Lost".encode() + str(turn).encode())
+                        break
+                    else:
+                        client.send("High".encode()+str(turn).encode())
+                if int(data) < num:
+                    if turn == 5:
+                        client.send("Lost".encode() + str(turn).encode())
+                        break
+                    else:
+                        client.send("Low".encode() + str(turn).encode())
+            except ValueError:
                 break
-            if int(data) > num:
-                if turn == 5:
-                    client.send("Lost".encode() + str(turn).encode())
-                    break
-                else:
-                    client.send("High".encode()+str(turn).encode())
-            if int(data) < num:
-                if turn == 5:
-                    client.send("Lost".encode() + str(turn).encode())
-                    break
-                else:
-                    client.send("Low".encode() + str(turn).encode())
-        except ValueError:
-            break
         print("Turn taken", turn)
+
+def hover_in(e):                                                         
+    e.widget["background"] = "#FA9F42"
+
+
+def hover_out(e):                                                         
+    e.widget["background"] = "#721817"
+
+
+main_win = Tk()
+main_win.geometry("300x600")
+main_win.resizable(0, 0)
+main_win.title("Server Monitor")
+
+canvas_main = Canvas(main_win, width=300, height=600,bg="black")
+canvas_main.pack(fill="both", expand=True)
+canvas_main.create_text(150, 15, text="Server", fill="#FA9F42",
+                        font="Times 18 bold",justify="center",anchor="n")
+start_btn = Button(canvas_main,text="start",command=start_server, bg="#721817", relief="groove",
+                activebackground="#721817")
+stop_btn = Button(canvas_main,text="stop",command=stop_server, bg="#721817", relief="groove",
+                activebackground="#721817")
+
+
+start_btn.place(x=125,y=50,anchor=NE)
+start_btn.bind("<Enter>", hover_in)
+start_btn.bind("<Leave>", hover_out)
+
+stop_btn.place(x=175,y=50,anchor=NW)
+stop_btn.bind("<Enter>", hover_in)
+stop_btn.bind("<Leave>", hover_out)
+
+
+
+main_win.mainloop()
+
+
+
+
+
+
+
 
 
